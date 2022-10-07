6be96d2d24a66147558d22258ec2947c78afa041
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-07 16:47:08 +0300

    task_13, task_15

---
+++ task_13/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("введите число ");
+int lm = 0;
+double N = Math.Abs(double.Parse(Console.ReadLine()));
+double n = N;
+// определяем есть-ли третья цифра?
+if ( n < 100 ){
+    Console.WriteLine("нет третьей цифры");
+}
+//
+// определяем длинну массива
+else {
+    while (n / 10 > 0 ){
+        n = (n - n % 10) / 10;
+        lm++;
+    }
+    Console.WriteLine( lm );
+//
+// заполняем массив
+    int[] a = new int[lm];
+    for (int i = 0; i < a.Length; i++){
+        a[i] = (int) N %10;
+        N = (N - N % 10)/10;
+    }
+    Console.WriteLine( a[lm-3] ); // печатаем третью цифру введёного числа
+    for (int i = 0; i < a.Length; i++){
+    Console.Write(a[i] + " ");   // печатаем весь массив
+    }
+}
---
+++ task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите номер дня недели ");
+int ndw = int.Parse(Console.ReadLine());
+if ( ndw < 1 || ndw >7)
+{
+    Console.WriteLine("Не корректный номер!!!");
+}
+else if ( ndw < 6)
+{
+    Console.WriteLine("Будний день");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
