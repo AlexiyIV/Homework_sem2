e98415f01406e9dbf86fbefa0a31bd866c92457f
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-07 13:43:32 +0300

    task_10_math

--- task_10_math/Program.cs
+++ task_10_math/Program.cs
@@ -2,6 +2,10 @@
 int N = Math.Abs(int.Parse(Console.ReadLine()));
 int n2 = 0;
 if ( N >= 100 && N < 1000 ){
-    n2 = ( N - N / 100 * 100 ) / 10; 
+    n2 = ( N - N / 100 * 100 ) / 10;
+    Console.WriteLine(n2);
 }
-Console.WriteLine(n2);
+else {
+    Console.WriteLine("число не трёхзначное");
+}
+
