758bb3cdb97b3dbdeed82645151c6fa9bd4e7809
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-07 13:36:34 +0300

    task_10_math

---
+++ task_10_math/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("введите трёхзначное число ");
+int N = Math.Abs(int.Parse(Console.ReadLine()));
+int n2 = 0;
+if ( N >= 100 && N < 1000 ){
+    n2 = ( N - N / 100 * 100 ) / 10; 
+}
+Console.WriteLine(n2);
